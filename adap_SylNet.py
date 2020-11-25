9f9e180b765350b336702045941242e5e2685b31
Shreyas Seshadri <shreyas.seshadri@aalto.fi>
2020-11-25 13:36:28 +0200

    Update adap_SylNet.py
    Fix to log issue

--- adap_SylNet.py
+++ adap_SylNet.py
@@ -110,7 +110,7 @@ for i in range(noUtt_main):
     fs, y = scipy.io.wavfile.read(fileList[i])
     y = y/max(abs(y))
     y = librosa.core.resample(y=y, orig_sr=fs, target_sr=Fs)
-    X[i] = np.transpose(20*np.log10(librosa.feature.melspectrogram(y=y, sr=Fs, n_mels=24, n_fft=w_l, hop_length=w_h)))
+    X[i] = np.transpose(20*np.log10(librosa.feature.melspectrogram(y=y, sr=Fs, n_mels=24, n_fft=w_l, hop_length=w_h)+0.00000000001))
 
 MEAN = np.load(means_path)  # Z norm data based on training data mean and std
 STD = np.load(std_path)
